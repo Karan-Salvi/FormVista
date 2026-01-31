e1beadc37a731e8f37efe00c2e1aea6343e942a7
Karan <karansalviwork@gmail.com>
2026-01-31 22:07:35 +0530

    feat(auth): add pages for the login and signup

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -6,6 +6,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Home/Index";
 import FormBuilder from "./pages/FormBuilder";
 import NotFound from "./pages/NotFound";
+import LoginPage from "./pages/Auth/Login";
+import SignUpPage from "./pages/Auth/Signup";
 
 const queryClient = new QueryClient();
 
@@ -18,6 +20,8 @@ const App = () => (
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/builder" element={<FormBuilder />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignUpPage />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
